a8baf3921d6cb4a009da638eb949e08bb7cf9f45
edos009 <dostyutyunnik@gmail.com>
2022-10-31 18:05:37 +0200

    Add new seeders for Bank (creator card)

--- server/src/seeders/123123123-Bank.js
+++ server/src/seeders/123123123-Bank.js
@@ -1,21 +1,31 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Banks', [
-      {
-        cardNumber: '4564654564564564',
-        name: 'SquadHelp',
-        expiry: '11/22',
-        cvc: '453',
-        balance: 0,
-      },
-      {
-        cardNumber: '4111111111111111',
-        name: 'yriy',
-        expiry: '09/23',
-        cvc: '505',
-        balance: 5000,
-      },
-    ], {});
+    return queryInterface.bulkInsert(
+      "Banks",
+      [
+        {
+          cardNumber: "4564654564564564",
+          name: "SquadHelp",
+          expiry: "11/22",
+          cvc: "453",
+          balance: 0,
+        },
+        {
+          cardNumber: "4111111111111111",
+          name: "buyer",
+          expiry: "09/23",
+          cvc: "505",
+          balance: 10000,
+        },
+        {
+          cardNumber: "4000000000000000",
+          name: "creator",
+          expiry: "09/23",
+          cvc: "505",
+          balance: 0,
+        },
+      ],
+      {}
+    );
   },
-
 };
